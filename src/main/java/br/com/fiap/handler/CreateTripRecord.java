912c7f639e10f0e9cbc5ed7b14d918fa26fbb272
Rafael Petronilio <rafaelpetronilio@gmail.com>
2020-05-06 17:52:35 -0300

    Trip DTO, README command

--- src/main/java/br/com/fiap/handler/CreateTripRecord.java
+++ src/main/java/br/com/fiap/handler/CreateTripRecord.java
@@ -2,15 +2,12 @@ package br.com.fiap.handler;
 
 import java.io.IOException;
 
-import br.com.fiap.model.PhotoBucket;
+import br.com.fiap.model.*;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.fiap.dao.TripRepository;
-import br.com.fiap.model.HandlerRequest;
-import br.com.fiap.model.HandlerResponse;
-import br.com.fiap.model.Trip;
 
 public class CreateTripRecord implements RequestHandler<HandlerRequest, HandlerResponse> {
 	
@@ -26,12 +23,15 @@ public class CreateTripRecord implements RequestHandler<HandlerRequest, HandlerR
 			return HandlerResponse.builder().setStatusCode(400).setRawBody("There is a error in your Trip!").build();
 		}
 
-		PhotoBucket photoBucket = new PhotoBucket(trip.getCountry(), trip.getCity(), trip.getDate());
+		PhotoBucket photoBucket = new PhotoBucket(trip.getCountry(), trip.getCity(), trip.getTripDate());
 		trip.setUrl(photoBucket.getName());
 
 		context.getLogger().log("Creating a new trip id, url: " + trip.getId()+", "+trip.getUrl());
 
 		final Trip tripRecorded = repository.save(trip);
-		return HandlerResponse.builder().setStatusCode(201).setObjectBody(tripRecorded).build();
+
+		CreateTripDTO tripDTO = new CreateTripDTO(trip.getId(),trip.getUrl());
+
+		return HandlerResponse.builder().setStatusCode(201).setObjectBody(tripDTO).build();
 	}
 }
---
+++ src/main/java/br/com/fiap/model/CreateTripDTO.java
@@ -0,0 +1,15 @@
+package br.com.fiap.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class CreateTripDTO {
+    private String id;
+    private String url;
+}
